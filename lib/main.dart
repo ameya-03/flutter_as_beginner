17c7a2bb26ab6106420f3c1bb58b125d9b5c8d8d
Amey <amey.nimkar1997@gmail.com>
2023-01-17 17:21:55 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
-import 'package:flutter_as_beginner/homepage.dart';
+import 'package:flutter_as_beginner/pages/homepage.dart';
+import 'package:flutter_as_beginner/pages/loginpage.dart';
 
 void main() {
   // ignore: prefer_const_constructors
@@ -15,7 +18,17 @@ class MyApp extends StatelessWidget {
 
     // ignore: prefer_const_constructors
     return MaterialApp(
+      // ignore: prefer_const_constructors
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      // ignore: prefer_const_literals_to_create_immutables, prefer_const_constructors
+      initialRoute: "/home",
+      routes: {
         // ignore: prefer_const_constructors
-        home: HomePage());
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
